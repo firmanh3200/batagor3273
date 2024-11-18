b12927e327ef2c51c72662e8bd615e1b1369d26e
firmanh3200 <135565052+firmanh3200@users.noreply.github.com>
2024-11-18 11:38:16 +0700

    Add files via upload

---
+++ Batagor.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+st.title('+++ B A T A G O R +++')
+st.subheader('Bandung Kota dalam :green[Grafik] dan :blue[Indikator]', divider='rainbow')
+st.success('Aplikasi ini berisi kumpulan Indikator Makro yang resmi dirilis oleh Badan Pusat Statistik.')
+st.info('Aplikasi ini dibuat untuk memudahkan Para Pemangku Kepentingan dalam proses Perencanaan, Pelaksanaan, \
+    Monitoring dan Evaluasi Pembangunan di Kota Bandung.')
+st.warning('Silakan mengakses setiap indikator makro melalui menu di sebelah kiri.')
+
+st.subheader('', divider='rainbow')
