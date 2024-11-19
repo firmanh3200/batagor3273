5e0ab593a278d0c5808fb69ea527867ae600f3a9
firmanh3200 <135565052+firmanh3200@users.noreply.github.com>
2024-11-19 21:07:53 +0700

    menambah penjelasan sumber data

--- Batagor.py
+++ Batagor.py
@@ -4,7 +4,8 @@ st.set_page_config(layout='wide')
 
 st.title('+++ B A T A G O R +++')
 st.subheader('Bandung Kota dalam :green[Grafik] dan :blue[Indikator]', divider='rainbow')
-st.success('Aplikasi ini berisi kumpulan Indikator Makro yang resmi dirilis oleh Badan Pusat Statistik.')
+st.success('Aplikasi ini berisi kumpulan Indikator Makro yang resmi dirilis oleh Badan Pusat Statistik, \
+    ditambah data-data lain yang sumber resminya tercantum.')
 st.info('Aplikasi ini dibuat untuk memudahkan Para Pemangku Kepentingan dalam proses Perencanaan, Pelaksanaan, \
     Monitoring dan Evaluasi Pembangunan di Kota Bandung.')
 st.warning('Silakan mengakses setiap indikator makro melalui menu di sebelah kiri.')
